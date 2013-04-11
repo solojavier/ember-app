9609b921782a16b4be3e0ec90e551550a3f4588e
Omar Rodriguez <omarjavier15@gmail.com>
2013-04-11 11:56:36 -0400

    Config routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,25 @@
 EmberApp::Application.routes.draw do
+
+  class FormatTest
+    attr_accessor :mime_type
+
+    def initialize format
+      @mime_type = Mime::Type.lookup_by_extension(format)
+    end
+
+    def matches? request
+      request.format == mine_type
+    end
+  end
+
+  resources :users, :except => :edit, 
+      :constraints => FormatTest.new(:json)
+
+  get '*foo', :to => 'ember#index',
+      :constraints => FormatTest.new(:html)
+  get '/', :to => 'ember#index',
+      :constraints => FormatTest.new(:html)
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
